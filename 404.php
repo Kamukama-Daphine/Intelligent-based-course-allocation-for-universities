fe02c24a531e546602fee8318aa2526c37c0513a
Kamukama Daphine <kamdmugisha@gmail.com>
2022-09-06 20:27:53 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INTELLIGENT BASED COURSE ALLOCATION FOR UNIVERSTIES</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="assets/js/lib/jquery-2.1.3.min.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+ <meta charset="UTF-8"> 
+        
+
+</head>
+<body>
+		<?php include "nav.php" ?>
+		<center><h1>Sorry Tracking Code Invalid or No Result Found</h1></center>
+
+		</body>
+		</html>
+
+
+
+		
---
+++ nav.php
@@ -0,0 +1,26 @@
+<div class="nava">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#008000;>
+  <a class="navbar-brand" href="#">
+      <img src="images/logo.png" width="70" height="50" class="d-inline-block align-top" alt=""> &nbsp &nbsp &nbsp &nbsp
+  <span>Intelligent Based Course Allocation For Universities</span><br><center><span>&nbsp &nbsp| Department of Computer Science </center></span>
+  </a>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav" style="background-color:green;">
+            <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white;">
+          Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="lec.php">Lecturer Login</a>
+          <a class="dropdown-item" href="index.php">Admin Login</a>
+          
+        </div>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
